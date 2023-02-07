7a224c92e9217f4b71cb967a00af437b3fadfca9
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-07 10:54:45 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "26de02bc-7f55-4d85-9172-1e9ee7eae3a3",
+    "neverthink_list": [
+        {
+            "id": "5335c46d-4e73-4fa6-aad3-e60ff1d19067",
+            "think_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png",
+            "hand_picked_text": "Speak piece different more. Them outside start start expert.",
+            "suggestion": "Actually position class PM worry company kind change. Put old radio television treat. Audience raise any even leader church director."
+        },
+        {
+            "id": "66ac1456-50be-40ba-b81a-a0da794c7e31",
+            "think_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png",
+            "hand_picked_text": "Only tell agent from air special a. Choose pass than east base long mouth. Realize person someone effect then.",
+            "suggestion": "Notice national state high the. Arrive play kitchen stage. Health energy total anyone."
+        },
+        {
+            "id": "ac9baafb-3866-4b85-a830-9cf816ee48ee",
+            "think_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png",
+            "hand_picked_text": "Modern mouth play section. Management president deal size step.",
+            "suggestion": "Member red national third relate other development. Country teacher that road anyone. Strategy describe theory happy explain floor."
+        },
+        {
+            "id": "78625db7-b730-4bc2-bfe3-e387b63a151f",
+            "think_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png",
+            "hand_picked_text": "Look any food. According yard sort song whole.",
+            "suggestion": "Key material land black us. These particularly court. System experience magazine sea respond newspaper beat."
+        },
+        {
+            "id": "9de5829b-820e-4acd-9887-56cab7fe4fca",
+            "think_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png",
+            "hand_picked_text": "Easy well point short low quality. Main music evidence wonder political. Give point real friend.",
+            "suggestion": "Able front rest about child likely recognize. Military special change themselves. Color economic moment research."
+        }
+    ]
+}
